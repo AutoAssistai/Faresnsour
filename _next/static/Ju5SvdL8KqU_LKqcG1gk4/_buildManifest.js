4bcf88d5f5ada4081a1a380a32cd3d926412374d
fares <fares@pop-os.home.arpa>
2025-11-21 09:07:34 +0300

    Updates

--- _next/static/Ju5SvdL8KqU_LKqcG1gk4/_buildManifest.js
+++ _next/static/Ju5SvdL8KqU_LKqcG1gk4/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/AutoAssistsite//_next/:path+",
-        "destination": "/AutoAssistsite/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
